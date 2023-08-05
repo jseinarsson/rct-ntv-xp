43e2bebb3921b1fd9b7a0e584db559dbbbda68d4
Jakob <69308518+jseinarsson@users.noreply.github.com>
2023-08-05 20:34:18 +0000

    search page

--- app/_layout.js
+++ app/_layout.js
@@ -18,7 +18,7 @@ const Layout = () => {
         }
     }, [fontsLoaded])
 
-    if(!fontsLoaded) return null;
+    if (!fontsLoaded) return null;
 
     return <Stack onLayout={onLayoutRootView} />;
 }
